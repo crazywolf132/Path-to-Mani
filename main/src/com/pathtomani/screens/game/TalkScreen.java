51f7ce643f7ae480db6a2644895bc9f8d72f42aa
crazywolf132 <crazywolf132@gmail.com>
2016-09-17 11:27:26 +1000

    Moving files to Managers.

--- main/src/com/pathtomani/screens/game/TalkScreen.java
+++ main/src/com/pathtomani/screens/game/TalkScreen.java
@@ -17,7 +17,7 @@
 package com.pathtomani.screens.game;
 
 import com.badlogic.gdx.math.Rectangle;
-import com.pathtomani.common.GameOptions;
+import com.pathtomani.managers.GameOptions;
 import com.pathtomani.ManiApplication;
 import com.pathtomani.gfx.ManiColor;
 import com.pathtomani.game.ManiGame;
