e3873af3f3dd85dd7347ae7ce7986bcde7335ef2
crazywolf132 <crazywolf132@gmail.com>
2016-09-17 11:25:00 +1000

    Moving files to Managers.

--- main/src/com/pathtomani/game/ObjectManager.java
+++ main/src/com/pathtomani/game/ObjectManager.java
@@ -20,6 +20,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.pathtomani.common.ManiMath;
+import com.pathtomani.managers.DebugOptions;
 import com.pathtomani.managers.dra.Dra;
 import com.pathtomani.managers.dra.FarDras;
 import com.pathtomani.common.Const;
